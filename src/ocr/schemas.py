9a26ecd56dbdb96a5552c990ffaadad3e0905077
NguyenThanhThiet87 <tn908261@gmail.com>
2026-05-26 10:56:45 +0700

    feat: add OCR business card to extract some field

--- src/ocr/schemas.py
+++ src/ocr/schemas.py
@@ -1,13 +1,11 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
-class OcrBlockSchema(BaseModel):
-    text: str
-    bbox: list[float]
-    confidence: float
-
-
-class VisionRegionSchema(BaseModel):
-    label: str
-    bbox: list[float]
-    confidence: float
+class BusinessCard(BaseModel):
+    name: str = Field(description="The name of the person")
+    phones: list[str] = Field(description="The phone number of the person")
+    email: str = Field(description="The email address of the person")
+    company: str = Field(description="The company name of the person")
+    position: str = Field(description="The position of the person")
+    address: str = Field(description="The address of the person")
+    website: str = Field(description="The website of the person")
--- tests/ocr/test_extract.py
+++ tests/ocr/test_extract.py
@@ -1,12 +1,17 @@
-# TODO(P4 — Cường, Thiệt): Implement OCR tests
-import pytest
+from datetime import datetime
+from src.ocr.service import pipline_ocr_to_llm
+import asyncio
 
+async def test_pipline_ocr_to_llm():
+    with open("src\ocr\sample\z7865925149626_e5b7e9e62ccb6c1254df42fac172b8cd.jpg", "rb") as f1, \
+         open("src\ocr\sample\z7865925149626_e5b7e9e62ccb6c1254df42fac172b8cd.jpg", "rb") as f2:
+        img_bytes1 = f1.read()
+        img_bytes2 = f2.read()
+    result = await pipline_ocr_to_llm([img_bytes1, img_bytes2])
+    print("LLM Result: ", result)
 
-@pytest.mark.asyncio
-async def test_ocr_passport_au():
-    pass
-
-
-@pytest.mark.asyncio
-async def test_vision_doc_type_detection():
-    pass
+if __name__ == "__main__":
+    try:
+        asyncio.run(test_pipline_ocr_to_llm())
+    except KeyboardInterrupt:
+        pass
