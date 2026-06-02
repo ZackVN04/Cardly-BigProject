1275369945e1ed65c3bde398ad503566fe447c52
tthqndvntc <nguyentronghieutc2004@gmail.com>
2026-06-02 20:59:04 +0700

    ci: integrate google cloud secret manager to fetch json key

--- docker/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-if [ -n "$FASTAPI_UPLOADER_JSON_B64" ]; then
-  echo "$FASTAPI_UPLOADER_JSON_B64" | base64 -d > /tmp/fastapi-uploader.json
-  export GOOGLE_APPLICATION_CREDENTIALS=/tmp/fastapi-uploader.json
-fi
-
-exec "$@"
